119a8506509bce2bd5be86f0af74be5adbc20067
Seppo Yli-Olli <seppo.yli-olli@iki.fi>
2018-08-20 00:07:15 +0300

    Revert "Skip copying over BAK directory"

--- steam_wrapper/steam_wrapper.py
+++ steam_wrapper/steam_wrapper.py
@@ -135,11 +135,10 @@ def migrate_data():
     source = os.path.expandvars("$XDG_DATA_HOME")
     target = ".data"
     steam_home = os.path.join(source, "Steam")
-    backup = os.path.join(source, "BAK")
     xdg_data_home = os.path.join(STEAM_ROOT, target)
 
     if not os.path.islink(source):
-        copytree(source, target, ignore=[steam_home, backup])
+        copytree(source, target, ignore=[steam_home])
         os.makedirs(target, exist_ok=True)
         if os.path.isdir(steam_home):
             os.rename(steam_home,
