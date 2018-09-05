b18f29e9b8a0466f9d9c4ee6522c04851ab4ca98
Seppo Yli-Olli <seppo.yli-olli+github@iki.fi>
2018-09-05 19:24:05 +0300

    Looks like with FDO 18.08 this variable leads into Steam start failures

--- steam_wrapper/steam_wrapper.py
+++ steam_wrapper/steam_wrapper.py
@@ -136,7 +136,6 @@ def legacy_support():
     if force_legacy or not check_nonempty("/etc/ld.so.conf"):
         # Fallback for flatpak < 0.9.99
         os.environ["LD_LIBRARY_PATH"] = "/app/lib:/app/lib/i386-linux-gnu"
-        os.environ["STEAM_RUNTIME_PREFER_HOST_LIBRARIES"] = "0"
 
     steam_home = os.path.expandvars("$HOME/.var/app/com.valvesoftware.Steam/home")
     if os.path.isdir(steam_home):
